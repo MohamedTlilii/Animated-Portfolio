04d7bc92863fb2fbbc1b68b5f7154183b688eafc
MohamedTlili <Mohamedtlili1995@hotmail.com>
2024-03-24 01:17:54 +0100

    parallax

--- src/components/Parallax/Parallax.jsx
+++ src/components/Parallax/Parallax.jsx
@@ -1,9 +1,20 @@
+import { useRef } from "react";
 import "./Parallax.scss";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 function Parallax({ type }) {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
   return (
     <div
       className="parallax"
+      ref={ref}
       style={{
         background:
           type === "services"
@@ -11,10 +22,12 @@ function Parallax({ type }) {
             : "linear-gradient(180deg, #11132, #505064)",
       }}
     >
-      <h1>{type === "services" ? "What We Do?" : "What We Did?"}</h1>
-      <div className="mountains"></div>
-      <div className="planets"></div>
-      <div className="stars"></div>
+      <motion.h1 style={{ y: yText }}>
+        {type === "services" ? "What We Do?" : "What We Did?"}
+      </motion.h1>
+      <motion.div className="mountains"></motion.div>
+      <motion.div style={{ y: yBg }} className="planets"></motion.div>
+      <motion.div style={{ x: yBg }} className="stars"></motion.div>
     </div>
   );
 }
